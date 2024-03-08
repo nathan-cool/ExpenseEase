731bb88f47a358784a431faa14943310189ca48c
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-03-08 11:25:55 +0000

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import environ
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,4 +131,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+
+django_heroku.settings(locals())  
