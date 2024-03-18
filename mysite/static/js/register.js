91357322b890325fe2a4f4bad777a77414faef6c
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-03-18 12:57:11 +0000

    JS valid code on register page

--- mysite/static/js/register.js
+++ mysite/static/js/register.js
@@ -1,4 +1,6 @@
 const users_name = document.getElementById('users_name');
+const email = document.getElementById('email');
+const users_password = document.getElementById('password');
 const feedback = document.getElementsByClassName('invalid-feedback');
 
 users_name.addEventListener('keyup', (e) => {
@@ -25,6 +27,72 @@ users_name.addEventListener('keyup', (e) => {
           users_name.classList.add('is-invalid');
           feedback[0].style.display = 'block';
           feedback[0].innerHTML = `<p>${data.users_name_error}</p>`;
+        }
+      }).catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+});
+
+
+
+email.addEventListener('keyup', (e) => {
+  const users_emailValue = e.target.value;
+
+  if (users_emailValue.length > 0) {
+    fetch('/authentication/validate-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: users_emailValue }),
+    }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('Success:', data);
+        if (data.email_valid) {
+          console.log('Success:', data);
+          email.classList.add('is-valid');
+          email.classList.remove('is-invalid');
+          feedback[1].style.display = 'none';
+        } else {
+          email.classList.remove('is-valid');
+          email.classList.add('is-invalid');
+          feedback[1].style.display = 'block';
+          feedback[1].innerHTML = `<p>${data.email_error}</p>`;
+        }
+      }).catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+});
+
+users_password.addEventListener('keyup', (e) => {
+  const users_passwordValue = e.target.value;
+
+  if (users_passwordValue.length > 0) {
+    fetch('/authentication/validate-password', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ password: users_passwordValue }),
+    }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('Success:', data);
+        if (data.users_password_valid) {
+          console.log('Success:', data);
+          users_password.classList.add('is-valid');
+          users_password.classList.remove('is-invalid');
+          feedback[2].style.display = 'none';
+        } else {
+          users_password.classList.remove('is-valid');
+          users_password.classList.add('is-invalid');
+          feedback[2].style.display = 'block';
+          feedback[2].innerHTML = `<p>${data.users_password_error}</p>`;
         }
       }).catch((error) => {
         console.error('Error:', error);
