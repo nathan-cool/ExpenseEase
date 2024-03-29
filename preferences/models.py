d86a5c7f37446cd751a06db01bfa4a821fdea020
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-03-29 11:10:06 +0000

    Refactor code and update preferences view

--- preferences/models.py
+++ preferences/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class UserPreferences(models.Model):
+  user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+  currency = models.CharField(max_length=255, default='EUR')
+
+  def __str__(self):
+    return str(self.user)+'s' 'preferences'
--- preferences/views.py
+++ preferences/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from django.views import View
+import os
+import json
+from django.conf import settings
+from .models import UserPreferences
+from django.contrib import messages
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "preferences/index.html")
+    exists = UserPreferences.objects.get(user=request.user)
+
+    if request.method == "GET":
+        currency_list = []
+
+        with open(os.path.join(settings.BASE_DIR, "currencies.json"), "r") as file:
+
+            data = json.load(file)
+
+        for k, v in data.items():
+            currency_list.append({"name": k, "value": v})
+
+        return render(request, "preferences/index.html", {"currencies": currency_list})
+    else:
+        currency = request.POST["currency"]
+        user_preferences.currency = currency
+        user_preferences.save()
+        messages.success(request, "Changes saved")
+        return render(request, "preferences/index.html", {"currencies": currency_list})
+    
+    
