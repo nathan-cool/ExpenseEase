7ca44411e99bfed1bf24199b3353fc55f127a642
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-04-08 13:58:54 +0100

    Refactor add_expenses view and template

--- expenses/views.py
+++ expenses/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Category, Expenses
+from django.shortcuts import redirect
+from django.contrib import messages
 
 
 # Create your views here.
 @login_required(login_url='/authentication/login')
 def index(request):
-    category = Category.objects.all()
+    categories = Category.objects.all()
     
     return render(request, 'expenses/index.html')
 
 def add_expenses(request):
-    category = Category.objects.all()
-    context = {
-        'category': category,
-    }
+    if request.method == "GET":
+        categories = Category.objects.all()
+        context = {
+            "categories": categories,
+        }
+        
+
+    if request.method == "POST":
+        amount = request.POST['amount']
+            
+        if not amount:
+            messages.error(request, "Amount is required")
+    
+        
     return render(request, "expenses/add-expenses.html", context)
+
+
