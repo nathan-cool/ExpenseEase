af12f9ddbc8933146275fa4b65c4bb8a270e1e7a
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-04-08 21:23:52 +0100

    Made changes too expenses URLs and views

--- expenses/urls.py
+++ expenses/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 
-
 urlpatterns = [
-    path('', views.index, name='expenses'),
-    path('add-expenses/', views.add_expenses, name='add-expenses'),
+    path("", views.index, name="expenses"),
+    path("add-expenses/", views.add_expenses, name="add-expenses"),
+    path("edit-expense/<int:id>", views.expense_edit, name="expense-edit"),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -9,8 +9,13 @@ from django.contrib import messages
 @login_required(login_url='/authentication/login')
 def index(request):
     categories = Category.objects.all()
-    
-    return render(request, 'expenses/index.html')
+    expenses = Expenses.objects.filter(owner=request.user)
+
+    context = {
+        "expenses": expenses,
+    }
+
+    return render(request, "expenses/index.html", context)
 
 def add_expenses(request):
     categories = Category.objects.all()
@@ -28,24 +33,23 @@ def add_expenses(request):
         category = request.POST["category"]
         description = request.POST["description"]
         owner = request.user
-        
 
         if not amount:
             messages.error(request, "Amount is required")
             return render(request, "expenses/add-expenses.html", context)
-        
+
         if not date:
             messages.error(request, "Date is required")
             return render(request, "expenses/add-expenses.html", context)
-        
+
         if not category:
             messages.error(request, "Category is required")
             return render(request, "expenses/add-expenses.html", context)
-        
+
         if not description:
             messages.error(request, "Description is required")
             return render(request, "expenses/add-expenses.html", context)
-        
+
         Expenses.objects.create(
             owner=owner,
             amount=amount,
@@ -56,3 +60,14 @@ def add_expenses(request):
         messages.success(request, "Expenses saved successfully")
         return redirect('expenses')
 
+
+def expense_edit(request, id):
+    expense = Expenses.objects.get(pk=id)
+
+    context = {"expense": expense, "values": expense}
+
+    if request.method == "GET":
+        return render(request, "expenses/edit-expense.html", context)
+    else:
+        messages.info(request, "Expense not edited")
+        return render(request, "expenses/edit-expense.html", context)
