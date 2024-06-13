1ccb3b0a89fd4f6ad3ce059c092e5689bb167ff1
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-06-13 12:42:15 +0100

    Revert "Fixed up HTML code"
    This reverts commit b8881b3293a612985541b9c47ab1777bc744c724.

--- preferences/views.py
+++ preferences/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 @login_required
 def index(request):
     try:
@@ -12,6 +13,9 @@ def index(request):
 
 def index(request):
 
+=======
+def index(request, *args):
+>>>>>>> parent of b8881b3 (Fixed up HTML code)
     currency_list = []
 
     with open(os.path.join(settings.BASE_DIR, "currencies.json"), "r") as file:
@@ -23,6 +27,7 @@ def index(request):
             currency_list.append({"name": k, "value": v})
 
     if exists:
+<<<<<<< HEAD
 >>>>>>> parent of 6352970 (chore: Clean up code formatting and remove unnecessary lines SearchExpenses.JS)
         user_preferences = UserPreferences.objects.get(user=request.user)
     except UserPreferences.DoesNotExist:
@@ -32,13 +37,39 @@ def index(request):
         user_preferences.save()
 
     if request.method == "GET":
+=======
+        user_preferences = UserPreferences.objects.get(user=request.user)
+
+    if request.method == "GET":
+
+>>>>>>> parent of b8881b3 (Fixed up HTML code)
         return render(
             request, "expenses/index.html", {"currency": user_preferences.currency}
         )
     else:
+<<<<<<< HEAD
         currency = request.POST.get("currency", "USD")
         if currency:
             user_preferences.currency = currency
             user_preferences.save()
             messages.success(request, "Currency preferences updated successfully.")
         return redirect("expenses:index")
+=======
+        currency = request.POST["currency"]
+
+        if exists:
+
+            user_preferences.currency = currency
+            user_preferences.save()
+        else:
+            user_preferences = UserPreferences.objects.create(user=request.user, currency=currency)
+        messages.success(request, "Changes saved")
+        return render(
+            request,
+            "preferences/index.html",
+            {"currencies": currency_list, "user_preferences": user_preferences},
+        )
+
+
+data = json.load(open(os.path.join(settings.BASE_DIR, "currencies.json"), "r"))
+>>>>>>> parent of b8881b3 (Fixed up HTML code)
