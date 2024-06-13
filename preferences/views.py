9cb48f5f03ab92c8aa8fdacf1d46c8db4dd57aea
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-06-13 11:45:53 +0100

    Refactor preferences/views.py

--- preferences/views.py
+++ preferences/views.py
@@ -1,54 +1,27 @@
-from django.shortcuts import render
-from django.views import View
-import os
-import json
-from django.conf import settings
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import UserPreferences
 from django.contrib import messages
-from django.core.exceptions import ObjectDoesNotExist
 
 
-def index(request, *args):
-    currency_list = []
-
-    with open(os.path.join(settings.BASE_DIR, "currencies.json"), "r") as file:
-        data = json.load(file)
-        for k, v in data.items():
-            currency_list.append({"name": k, "value": v})
-
+@login_required
+def index(request):
     try:
         user_preferences = UserPreferences.objects.get(user=request.user)
     except UserPreferences.DoesNotExist:
-        user_preferences = None
+        user_preferences = UserPreferences.objects.create(
+            user=request.user, currency="USD"
+        )
+        user_preferences.save()
 
     if request.method == "GET":
-        if user_preferences is None:
-            user_preferences = UserPreferences.objects.create(
-                user=request.user, currency="USD"
-            )
-
         return render(
-            request,
-            "preferences/index.html",
-            {"currencies": currency_list, "user_preferences": user_preferences},
+            request, "expenses/index.html", {"currency": user_preferences.currency}
         )
     else:
-        currency = request.POST.get("currency")
-
-        if user_preferences:
+        currency = request.POST.get("currency", "USD")
+        if currency:
             user_preferences.currency = currency
             user_preferences.save()
-        else:
-            user_preferences = UserPreferences.objects.create(
-                user=request.user, currency=currency
-            )
-
-        messages.success(request, "Changes saved")
-        return render(
-            request,
-            "preferences/index.html",
-            {"currencies": currency_list, "user_preferences": user_preferences},
-        )
-
-
-data = json.load(open(os.path.join(settings.BASE_DIR, "currencies.json"), "r"))
+            messages.success(request, "Currency preferences updated successfully.")
+        return redirect("expenses:index")
