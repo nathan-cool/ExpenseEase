f0b52add059170c88d9f83646ab05b36b567c572
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-06-13 19:48:01 +0100

    Refactor JavaScript code for better organization and improve event handling in main.js and edit-expense.html

--- mysite/static/js/main.js
+++ mysite/static/js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello from main.js');
 
-  const expense_dsc = document.getElementById('expense_dsc');
+  const expense_dsc = document.getElementsByClassName('expense_dsc');
 
   expense_dsc.addEventListener('click', () => {
     expense_dsc.innerHTML = 'Hello from main.js';
