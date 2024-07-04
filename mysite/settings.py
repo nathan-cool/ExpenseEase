8340baa6c01608778a8789aa3d36788c747c2ad1
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-07-04 23:17:29 +0100

    Updated Dashboard CSS with more comments and deleted unused files

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,10 +32,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@_^h%@0sy-@cyhv4e3n_^x_s+%8pq4481)97*)921)oi8$+m$)"
+SECRET_KEY = os.getenv("SECRET_KEY"),
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
